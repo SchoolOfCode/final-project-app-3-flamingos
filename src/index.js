5b822f651dac87b7336d465ef30e41d6e2c7c13d
Jonathan Lester <jonathanmarklester@gmail.com>
2019-04-30 14:37:03 +0000

    setup react routing and pages folder. components should be individual components, these will be used to build pages/views in the pages folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Import pages
+import Home from "./pages/Home";
 
+import * as serviceWorker from "./serviceWorker";
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const routing = (
+    <Router>
+        <Route exact path="/" component={Home} />
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
