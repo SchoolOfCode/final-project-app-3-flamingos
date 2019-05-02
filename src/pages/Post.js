21e5762a0673c414bb7884dc70f6ebb52340f6a4
ropenfold <stuartroper@gmail.com>
2019-05-02 15:32:42 +0100

    add logo

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -2,16 +2,17 @@ import React from "react";
 import CurrentLocation from "../components/CurrentLocation";
 import PostForm from "../components/PostForm";
 import Logo from "../components/Logo";
+
 // import css from "./cssModules/post.module.css";
 import "../index.css";
 
 const Post = props => {
-  return (
-    <div id="post">
-      <Logo />
-      <CurrentLocation />
-      <PostForm />
-    </div>
-  );
-};
+    return (
+        <div id="post">
+            <Logo />
+            <CurrentLocation />
+            <PostForm />
+        </div>
+    );
+}
 export default Post;
