99e245da97d32bed1b63ad077d6aeefb07f0a594
Jonathan Lester <jonathanmarklester@gmail.com>
2019-05-02 13:04:17 +0000

    all form components now controlled by react and submit outputs to console.log)

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,13 +1,27 @@
-import React from "react";
-import CurrentLocation from "../components/CurrentLocation";
+import React, { useState, useEffect } from "react";
+import Location from "../components/Location";
 import PostForm from "../components/PostForm";
+import Nav from "../components/Nav";
 
 const Post = props => {
+    const [location, setLocation] = useState({});
+    // const [zoom, setZoom] = useState(props.zoom);
+
+    useEffect(() => {
+        if (navigator && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(pos => {
+                setLocation({
+                    lat: pos.coords.latitude,
+                    long: pos.coords.longitude
+                });
+            });
+        }
+    }, []);
     return (
         <div>
-            <h1>Post</h1>
-            <CurrentLocation />
-            <PostForm />
+            <Location zoom={11} lat={location.lat} long={location.long} />;
+            <PostForm lat={location.lat} long={location.long} />
+            <Nav />
         </div>
     );
 };
