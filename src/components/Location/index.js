5e777f986702b6dd8daa1fee2b8b7a5aa8acd803
Jonathan Lester <jonathanmarklester@gmail.com>
2019-05-02 16:34:36 +0000

    added sb_ envs

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import css from "./Location.module.css";
 
 import GoogleMapReact from "google-map-react";
-
-console.log({ gKey: process.env.REACT_APP_GMAPS_KEY });
+const APIkey =
+    process.env.REACT_APP_GMAPS_KEY || process.env.STORYBOOK_GMAPS_KEY;
 
 const Location = props => {
     return (
         <div className={css.container}>
             <GoogleMapReact
                 bootstrapURLKeys={{
-                    key: process.env.REACT_APP_GMAPS_KEY
+                    key: APIkey
                 }}
                 center={{ lat: props.lat, lng: props.long }}
                 zoom={props.zoom}
