70183d454234240df8fed1d6d89d4e786061995e
jonathanmarklester <jonathanmarklester@gmail.com>
2019-05-10 14:45:35 +0100

    added new page to story book

--- src/pages/pages.stories.js
+++ src/pages/pages.stories.js
@@ -2,20 +2,15 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import Home from "./Home.js";
-import Post from "./Post.js";
 import Live from "./Live.js";
-import Register from "./Register.js";
 import Login from "./Login.js";
+import New from "./New.js";
+import Post from "./Post.js";
+import Register from "./Register.js";
 
 storiesOf("Pages|Home", module)
     .add("Desktop", () => <Home />)
     .add("Mobile", () => <Home />, {
-        viewport: { defaultViewport: "iphone5" }
-    });
-
-storiesOf("Pages|Post", module)
-    .add("Desktop", () => <Post />)
-    .add("Mobile", () => <Post />, {
         viewport: { defaultViewport: "iphone5" }
     });
 
@@ -25,14 +20,26 @@ storiesOf("Pages|Live", module)
         viewport: { defaultViewport: "iphone5" }
     });
 
-storiesOf("Pages|Register", module)
-    .add("Desktop", () => <Register />)
-    .add("Mobile", () => <Register />, {
-        viewport: { defaultViewport: "iphone5" }
-    });
-
 storiesOf("Pages|Login", module)
     .add("Desktop", () => <Login />)
     .add("Mobile", () => <Login />, {
+        viewport: { defaultViewport: "iphone5" }
+    });
+
+storiesOf("Pages|New", module)
+    .add("Desktop", () => <New />)
+    .add("Mobile", () => <New />, {
+        viewport: { defaultViewport: "iphone5" }
+    });
+
+storiesOf("Pages|Post", module)
+    .add("Desktop", () => <Post />)
+    .add("Mobile", () => <Post />, {
+        viewport: { defaultViewport: "iphone5" }
+    });
+
+storiesOf("Pages|Register", module)
+    .add("Desktop", () => <Register />)
+    .add("Mobile", () => <Register />, {
         viewport: { defaultViewport: "iphone5" }
     });
