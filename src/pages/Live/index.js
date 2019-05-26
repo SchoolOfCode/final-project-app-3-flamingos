4be010d2e303c2e8bf2ff7aee0201f6d47015ddf
Codeama <bukola_jimoh@yahoo.co.uk>
2019-05-26 02:40:13 +0100

    removed empty array argument in useEffect for fetch request as it was causing dates to be invalid

--- src/pages/Live/index.js
+++ src/pages/Live/index.js
@@ -41,7 +41,7 @@ const Live = props => {
         } catch (err) {
             console.error({ fetch: err });
         }
-    });
+    }); 
 
     useEffect(() => {
         socket.on("post", post => {
@@ -49,6 +49,7 @@ const Live = props => {
         });
     }, []);
 
+ 
     return isLoggedIn ? (
         <div className={css.mainContainer}>
             <MobileHeader />
@@ -72,7 +73,7 @@ const Live = props => {
                             const newTime = new Date(item.updatedAt)
                             item.createdAt = newDate.toDateString()
                             item.updatedAt = newTime.toTimeString().slice(0, 8)
-                            console.log(newDate)
+                            console.log(newTime)
                             return <SinglePost post={item}/>;
                         })}
                     </div>
