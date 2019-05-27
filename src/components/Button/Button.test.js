eb89a9f10730ba6da0998ebaa9cdb19ad14e5c06
Codeama <bukola_jimoh@yahoo.co.uk>
2019-05-27 21:03:25 +0100

    unit tests for Button component

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import React from 'react';
+import Button from './index';
+import {shallow, mount} from 'enzyme';
+
+
+//const wrapper = shallow(<Button />);
+
+describe('<Button/>', () => {
+    it('renders without crashing', () => {
+        shallow(<Button/>); 
+    });
+
+    it('should display exact text passed to it as a prop on a <button>', () => {
+        const wrapper = shallow(<Button buttonName="Test display"/>);
+        expect(wrapper.find('button').text()).toBe("Test display");
+    });
+
+    it('should handle click events registered to it on a single click', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = shallow(<Button onClick={mockCallBack}/>);
+        wrapper.find('button').simulate('click');
+        expect(mockCallBack).toHaveBeenCalledTimes(1);
+    });
+
+    it('should handle click events registered to it on a single click', () => {
+        const mockCallBack = jest.fn(() => 'Hello!');
+        const wrapper = shallow(<Button onClick={mockCallBack}/>);
+        wrapper.find('button').simulate('click');
+        expect(mockCallBack).toHaveBeenCalled()
+    })
+})
