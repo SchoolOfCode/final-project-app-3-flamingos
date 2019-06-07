c8411c0def256a4335cdaf372ba43fc15fa91948
jonathanmarklester <jonathanmarklester@gmail.com>
2019-06-07 10:29:02 +0100

    updated storybook

--- src/components/Comments/Comments.stories.js
+++ src/components/Comments/Comments.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from "@storybook/react";
 
 import Comments from ".";
 
-storiesOf("Comments", module).add("Comments", () => <Comments />);
+storiesOf("Components|Comments", module).add("Comments", () => <Comments />);
 
